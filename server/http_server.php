cd65410c7bb0e1ce5d560549dbb0d1b555a978ba
姚照明 <798314049@qq.com>
2018-07-09 17:59:43 +0800

    优化 server/http_server.php

--- server/http_server.php
+++ server/http_server.php
@@ -8,9 +8,19 @@ $http->set(
 	[
 		'enable_static_handler' => true,
 		'document_root' => "/home/vagrant/code/think/public/static/home",
+		'worker_num' => 4,
 	]
 );
+$http->on('WorkerStart', function ($serv, $worker_id) {
+	// 加载基础文件
+	require __DIR__ . '/../thinkphp/base.php';
+});
 $http->on('request', function ($request, $response) {
+	if (isset($resquest->server)) {
+		foreach ($resquest->server as $k => $v) {
+			$_SERVER[strtoupper($k)] = $v;
+		}
+	}
 	$response->end("guanguans" . json_encode($request->get));
 });
 
